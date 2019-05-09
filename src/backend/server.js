7c8d91dd59fcbb06fe0a5a0972e62573e6f3e947
sugaroverflow <sugaroverflow@gmail.com>
2019-05-09 10:27:00 -0400

    Add back the access control header to server.js.

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -7,5 +8,16 @@ app.use('/api/modules', require('./routes/module.router'));
 app.use('/api/phases', require('./routes/phase.router'));
 app.use('/api/tasks', require('./routes/task.router'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('dist'));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
