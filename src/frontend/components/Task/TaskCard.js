e3f03e8843763cc25e0da219487e03e8f78878d2
sugaroverflow <sugaroverflow@gmail.com>
2019-05-27 16:10:58 -0400

    Remove material ui elements from task card.

--- src/frontend/components/Task/TaskCard.js
+++ src/frontend/components/Task/TaskCard.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import { withStyles } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
 
-const styles = {
-  // card: {
-  //   maxWidth: '18rem'
-  // }
-};
 
 const TaskCard = ({ task }) => (
-  // <Grid item xs={4}>
-
   <div className="task-card">
     <h2 className="h6">{task.titleEng}</h2>
     <div className="card-image-task">
       <img className="card-task-img" src={task.bannerUrl} alt="Placeholder" />
     </div>
     <p className="card-text">{task.descEng}</p>
-    {/* @todo create proper NavLink */}
     <div className="card-footer">
       <NavLink className="btn btn-primary" to={`/Steps/${task.task_id}`}>
         Begin Task
       </NavLink>
     </div>
   </div>
-  // </Grid>
 );
 
 TaskCard.propTypes = {
@@ -38,4 +27,4 @@ TaskCard.propTypes = {
     descFra: PropTypes.string.isRequired
   }).isRequired
 };
-export default withStyles(styles)(TaskCard);
+export default TaskCard;
