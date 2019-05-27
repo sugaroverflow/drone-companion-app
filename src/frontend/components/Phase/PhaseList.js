65ba34b9b888c91a7eeafc88f66f6e11218caa5a
sugaroverflow <sugaroverflow@gmail.com>
2019-05-27 16:04:22 -0400

    Pairing with @andeepittman to update the phase card styles and markup.

--- src/frontend/components/Phase/PhaseList.js
+++ src/frontend/components/Phase/PhaseList.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import PhaseCard from './PhaseCard';
 
 const getPhases = phases => (
-  <Grid container spacing={24}>
+  <div className="phases-accordion">
     {phases.phases.map(phase => (
       <PhaseCard key={phase.phase_id} phase={phase} />
     ))}
-  </Grid>
+  </div>
 );
 
 const PhaseList = phases => <div>{getPhases(phases)}</div>;
