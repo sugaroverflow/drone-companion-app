369c62318d6fd2e990ab463421ba2b5cd4221540
sugaroverflow <sugaroverflow@gmail.com>
2019-06-04 10:50:26 -0400

    Remove node-sass and minicssextract plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const outputDirectory = 'build';
 
@@ -23,13 +22,8 @@ module.exports = {
         }
       },
       {
-        test: /\.(s*)css$/,
-        use: [
-          // fallback to style-loader in development
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(jpe?g|png|gif)$/,
@@ -66,12 +60,6 @@ module.exports = {
     }),
     new CopyWebpackPlugin([
       { from: './public/images', to: 'images' }
-    ]),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
+    ])
   ]
 };
