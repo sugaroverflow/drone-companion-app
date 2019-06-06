f41acd524dcb406809c9bf5616bedf245e667452
sugaroverflow <sugaroverflow@gmail.com>
2019-06-06 12:37:48 -0400

    Importing NavLink for taskStep.

--- src/frontend/components/Phase/TaskStep.js
+++ src/frontend/components/Phase/TaskStep.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { NavLink } from 'react-router-dom';
 
 const TaskStep = ({ task, moduleOId, phaseOId }) => (
   <li className="StepProgress-item">
