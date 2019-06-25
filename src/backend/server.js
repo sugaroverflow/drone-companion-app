79c174d09826b2c9e2c13207763734e2992f1ddf
sugaroverflow <sugaroverflow@gmail.com>
2019-06-25 18:12:33 -0400

    Revert "#212: fixing the pathname to index for CSR issue."
    This reverts commit f757525250e1f4c2ae69044c037f840998d519d4.

--- src/backend/server.js
+++ src/backend/server.js
@@ -20,7 +20,7 @@ app.use(express.static('build'));
 
 // serve index.html for all other routes
 app.get('/*', (req, res) => {
-  res.sendFile(path.join('build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 if (process.env.NODE_ENV === 'production') {
