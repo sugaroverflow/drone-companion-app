55c86537be5e263c652c84c6b71edf21208f8a00
Weiguang Xiao <44439424+xiaoweiguang@users.noreply.github.com>
2019-07-05 02:59:06 -0400

    Modified changelang in HOC

--- src/frontend/components/Phase/Phases.js
+++ src/frontend/components/Phase/Phases.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
-import PhaseList from './PhaseList';
-import withHeaderFooter from '../../common/withHeaderFooter';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withTranslation } from "react-i18next";
+import PhaseList from "./PhaseList";
+import withHeaderFooter from "../../common/withHeaderFooter";
 
 class Phases extends Component {
   constructor() {
     super();
     this.state = {
-      module: null,
+      module: null
     };
   }
 
   componentDidMount() {
-    const { lang } = this.props;
+    const { lang, onMounted } = this.props;
     this.loadData(lang);
+    if (onMounted) {
+      onMounted({
+        changeLang: lang => this.changeLang(lang)
+      });
+    }
   }
 
-  loadData = (lang) => {
-    console.log(lang);
+  loadData = lang => {
     fetch(`/api/phases?lang=${lang}`)
       .then(res => res.json())
-      .then((module) => {
+      .then(module => {
         this.setState({ module });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
-  }
+  };
 
   changeLang(lang) {
     this.loadData(lang);
@@ -35,23 +39,24 @@ class Phases extends Component {
 
   render() {
     const { module } = this.state;
+    const { i18n } = this.props;
     if (module) {
       return (
         <div className="App">
-          <h1 className="h4">
-            {`Module: ${module.title}`}
-          </h1>
+          <h1 className="h4">{`Module: ${module.title}`}</h1>
 
-          <PhaseList phases={module.Phases} />
+          <PhaseList phases={module.Phases} i18n={i18n} />
         </div>
       );
     }
-    return '';
+    return "";
   }
 }
 
 Phases.propTypes = {
-  lang: PropTypes.string.isRequired,
+  lang: PropTypes.string.isRequired
 };
 
-export default withTranslation('translation')(withHeaderFooter(Phases, 'Conducting Site Surveys'));
+export default withTranslation("phase")(
+  withHeaderFooter(Phases, "Conducting Site Surveys")
+);
