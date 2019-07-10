79c656db9a78270b40a37b3f1a54c53cb9787ad9
jnnfrpyn <jnnfrpyn@gmail.com>
2019-07-10 13:40:35 -0400

    Create privacy.js file

---
+++ src/frontend/pages/privacy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>
+      The
+      {' '}
+      <a href="https://github.com/sugaroverflow/drone-companion-app">
+      Drone Companion App
+      </a>
+      {' '}
+      is a Code for Canada + Transport Canada collaboration, aimed to help drone pilots fly safely.
+    </p>
+
+  </div>
+);
+
+export default About;
