b3263c1187ca0d8d0d69b7399604a76a4f77252c
jnnfrpyn <jnnfrpyn@gmail.com>
2019-07-11 10:49:54 -0400

    Update Privacy.js

--- src/frontend/pages/Privacy.js
+++ src/frontend/pages/Privacy.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const About = () => (
+const Privacy = () => (
   <div>
-    <h1>About</h1>
-    <p>
-      Privacy
-Employment and Social Development Canada (ESDC) is responsible for the administration of the Government of Canada website, Canada.ca, and for providing related services to the federal departments and agencies that publish their content to Canada.ca. ESDC is committed to respecting the privacy and protecting the information of all visitors to Canada.ca. ESDC’s administration of Canada.ca includes various activities that may require the collection of personal information for both administrativeote 1 and non-administrativeFootnote 2 purposes.
-All personal information collected by ESDC about visitors to Canada.ca is authorized under legislation and is done in compliance with the Privacy Act. This means that you will be informed when your personal information may be required, the purpose for which it is collected, and how to exercise your right of access to that information. While visiting Canada.ca, you may be directed to websites controlled by other Government of Canada institutions or a third-party, or to third-party social media platforms. When visiting a website outside of Canada.ca or a social media platform, personal information will be subject to the terms, conditions, and privacy policies of that other institution or third-party. In these cases, we strongly encourage you to take the time to read the specific privacy notices or statements that describe their specific terms, conditions, and policies. If you have any questions, comments, concerns or complaints about the activities of ESDC in administering the Canada.ca website as they relate to the Privacy Act, the Department of Employment and Social Development Act, and privacy policies regarding Canada.ca, you can contact the ESDC Access to Information and Privacy Coordinator. In addition you may also contact the Office of the Privacy Commissioner by telephone at 1-800-282-1376.
+    <h1>Privacy</h1>
+  <p>
+  Employment and Social Development Canada (ESDC) is responsible for the administration of the Government of Canada website, Canada.ca, and for providing related services to the federal departments and agencies that publish their content to Canada.ca. ESDC is committed to respecting the privacy and protecting the information of all visitors to Canada.ca. ESDC’s administration of Canada.ca includes various activities that may require the collection of personal information for both administrativeote 1 and non-administrativeFootnote 2 purposes. All personal information collected by ESDC about visitors to Canada.ca is authorized under legislation and is done in compliance with the Privacy Act. This means that you will be informed when your personal information may be required, the purpose for which it is collected, and how to exercise your right of access to that information. While visiting Canada.ca, you may be directed to websites controlled by other Government of Canada institutions or a third-party, or to third-party social media platforms. When visiting a website outside of Canada.ca or a social media platform, personal information will be subject to the terms, conditions, and privacy policies of that other institution or third-party. In these cases, we strongly encourage you to take the time to read the specific privacy notices or statements that describe their specific terms, conditions, and policies. If you have any questions, comments, concerns or complaints about the activities of ESDC in administering the Canada.ca website as they relate to the Privacy Act, the Department of Employment and Social Development Act, and privacy policies regarding Canada.ca, you can contact the ESDC Access to Information and Privacy Coordinator. In addition you may also contact the Office of the Privacy Commissioner by telephone at 1-800-282-1376.
     </p>
 
   </div>
 );
 
-export default About;
+export default Privacy;
